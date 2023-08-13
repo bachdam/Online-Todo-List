2883756bd0a367f4cff4c72fa2f5f51384736b51
bachdam <131636477+bachdam@users.noreply.github.com>
2023-08-12 19:11:17 -0500

    create new todo tasks

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
+let newTasks = [];
+
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   const date = new Date();
@@ -17,8 +20,15 @@ app.get("/", (req, res) => {
 
   let day = date.toLocaleString("en-US", options);
   console.log(day);
-  res.render("index.ejs", { currentDay: day });
+  res.render("index.ejs", { currentDay: day, newItems: newTasks });
 });
+
+app.post("/", (req, res) => {
+  let newTask = req.body.newItem;
+  newTasks.push(newTask);
+  res.redirect("/");
+});
+
 app.listen(port, () => {
   console.log(`>>> The website has been launched in port: ${port}`);
 });
